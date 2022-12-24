9bf5ec5cc29235e60d40dd61298224ecbd22a83d
Sk Ajhar Uddin <62983361+SKAJHARUDDIN@users.noreply.github.com>
2022-12-24 10:32:10 +0530

    added if else

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
 let name = prompt('Please enter your name : ');
 
-setTimeout(() => {
-	document.querySelector('#para0').innerHTML = `Dear ${name}, you are in right place for me. Thank you.`;
-}, 500);
-
-setTimeout(() => {
-    document.querySelector('#para1').innerHTML = `We are hacking your device...Please wait...`;
-}, 2500);
-
-setTimeout(() => {
-    document.querySelector('#para2').innerHTML = `We are fetching your all username and passwords in this device...Please wait...`;
-}, 4000);
-
-setTimeout(() => {
-    document.querySelector('#para3').innerHTML = `Hacking your Facebook Id...Please wait...`;
-}, 6500);
-
-setTimeout(() => {
-    document.querySelector('#fcbk').innerHTML = `Fetching your Facebook messenger...Please wait...`;
-}, 9500)
-
-setTimeout(() => {
-    document.querySelector('#para4').innerHTML = 'Hacking your Gmail...Please wait...';
-}, 12500);
-
-setTimeout(() => {
-    document.querySelector('#para5').innerHTML = 'Feching your WhatsApp chat...Please wait...';
-}, 16000);
-
-setTimeout(() => {
-    document.querySelector('#para6').innerHTML = `Dear ${name}, Congratulations myself!!! We got everything which needed. Thanks for clicking this link...And one more step...Please wait...`;
-}, 19000);
-
-setTimeout(() => {
-    document.querySelector('#para7').innerHTML = `Dear ${name}, kaise laga meri mazak...?🤣🤣🤣😂😂😂`;
-}, 21000);
+if(name){
+    setTimeout(() => {
+        document.querySelector('#para0').innerHTML = `Dear ${name}, you are in right place for me. Thank you.`;
+    }, 500);
+    
+    setTimeout(() => {
+        document.querySelector('#para1').innerHTML = `We are hacking your device...Please wait...`;
+    }, 2500);
+    
+    setTimeout(() => {
+        document.querySelector('#para2').innerHTML = `We are fetching your all username and passwords in this device...Please wait...`;
+    }, 4000);
+    
+    setTimeout(() => {
+        document.querySelector('#para3').innerHTML = `Hacking your Facebook Id...Please wait...`;
+    }, 6500);
+    
+    setTimeout(() => {
+        document.querySelector('#fcbk').innerHTML = `Fetching your Facebook messenger...Please wait...`;
+    }, 9500)
+    
+    setTimeout(() => {
+        document.querySelector('#para4').innerHTML = 'Hacking your Gmail...Please wait...';
+    }, 12500);
+    
+    setTimeout(() => {
+        document.querySelector('#para5').innerHTML = 'Feching your WhatsApp chat...Please wait...';
+    }, 16000);
+    
+    setTimeout(() => {
+        document.querySelector('#para6').innerHTML = `Dear ${name}, Congratulations myself!!! We got everything which needed. Thanks for clicking this link...And one more step...Please wait...`;
+    }, 19000);
+    
+    setTimeout(() => {
+        document.querySelector('#para7').innerHTML = `Dear ${name}, kaise laga meri mazak...?🤣🤣🤣😂😂😂`;
+    }, 21000);
+} else {
+    alert('Please enter your name.');
+}
